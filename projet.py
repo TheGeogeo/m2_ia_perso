a55778e057949aaa815804b62648d92c59a4a582
TheGeogeo <coudrier.g@gmail.com>
2025-01-07 15:29:53 +0100

    up

--- projet.py
+++ projet.py
@@ -3,5 +3,10 @@ import pandas as pd
 df = pd.read_csv('sales_and_customer_insights.csv')
 
 print(df.head())
+print("--------")
 print(df.info())
+print("--------")
+print(df.info(memory_usage='deep'))
+print("--------")
 print(df.describe())
+print("--------")
